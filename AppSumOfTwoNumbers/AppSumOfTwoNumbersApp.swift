8895121b139e9bc4d5c65bad9e02a815f5565909
vdnk_d <dmitryvdvnk@gmail.com>
2024-07-03 17:29:05 +0300

    Initial Commit

---
+++ AppSumOfTwoNumbers/AppSumOfTwoNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSumOfTwoNumbersApp.swift
+//  AppSumOfTwoNumbers
+//
+//  Created by Dmitry Vdovenko on 03.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppSumOfTwoNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
